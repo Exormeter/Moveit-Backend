dad92fc4f29ebf57b0010f49c22a5bee70727225
Tobias321 <tgrothe@fh-bielefeld.de>
2017-05-26 20:16:33 +0200

    Use cors whitelist 2

--- src/server.ts
+++ src/server.ts
@@ -193,16 +193,16 @@ export class Server {
         });
 
         // Use cors
-        var whitelist = ['http://localhost:8100', 'https://moveit-backend.herokuapp.com']
-var corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  }
-}
+        var whitelist = ['http://localhost', 'https://moveit-backend.herokuapp.com']
+        var corsOptions = {
+            origin: function (origin, callback) {
+                if (whitelist.indexOf(origin) !== -1) {
+                    callback(null, true)
+                } else {
+                    callback(new Error('Not allowed by CORS'))
+                }
+            }
+        }
         app.options('*', cors(corsOptions));
         app.use(cors(corsOptions));
 
