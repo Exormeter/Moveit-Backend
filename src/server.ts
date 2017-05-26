9446955d785a1fd85a1fe5e0e9807c439cfbc4ab
Tobias321 <tgrothe@fh-bielefeld.de>
2017-05-26 21:34:09 +0200

    Not use cors 2

--- src/server.ts
+++ src/server.ts
@@ -208,13 +208,13 @@ export class Server {
         app.use(function (req, res, next) {
             res.header('Access-Control-Allow-Credentials', true);
             res.header('Access-Control-Allow-Origin', req.headers.origin);
-            res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-            res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-            if ('OPTIONS' == req.method) {
+            // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+            // res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+            /* if ('OPTIONS' == req.method) {
                 res.send(200);
             } else {
                 next();
-            }
+            } */
         });
 
         // catch 404 and forward to error handler
