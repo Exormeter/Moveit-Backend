359edbf4c5e3e51d9408c7bb4fc88424bc535fff
Tobias321 <tgrothe@fh-bielefeld.de>
2017-05-26 22:24:24 +0200

    Not use cors 6

--- src/server.ts
+++ src/server.ts
@@ -210,7 +210,7 @@ export class Server {
             res.header('Access-Control-Allow-Origin', req.headers.origin);
             res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
             res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
+            next();
             // intercept OPTIONS method
             /*
             if ('OPTIONS' == req.method) {
